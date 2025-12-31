10af513162469df3acee93ea14a317ba11cb3fd8
Seongjung Um <232974616+dragon6217@users.noreply.github.com>
2026-01-01 08:15:02 +0900

    s04_l21 dags_python_with_op_args

---
+++ dags/s04_l21_dags_python_with_op_args.py
@@ -0,0 +1,19 @@
+import pendulum
+from common.common_func import regist
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+
+with DAG(
+    dag_id="s04_l21_dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    regist_t1 = PythonOperator(
+        task_id='regist_t1',
+        python_callable=regist,
+        op_args=['hjkim','man','kr','seoul']
+    )
+
+    regist_t1
--- plugins/common/s04_l18_common_func.py
+++ plugins/common/s04_l18_common_func.py
@@ -1,11 +1,14 @@
+#s04_l18
 def get_sftp():
     print('sftp 작업을 시작합니다')
     
 
+#s04_l21
 def regist(name, sex, *args):
     print(f'이름: {name}')
     print(f'성별: {sex}')
     print(f'기타옵션들: {args}')
+
 
 
 def regist2(name, sex, *args, **kwargs):
