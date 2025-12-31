c49302e008333e942b4ec78435d1ad530e25121f
Seongjung Um <232974616+dragon6217@users.noreply.github.com>
2026-01-01 03:39:24 +0900

    fix dags_conn_test.py

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -15,22 +15,22 @@ with DAG(
         task_id="empty_t2",
     )
     empty_t3 = EmptyOperator(
-        task_id="empty_t1",
+        task_id="empty_t3",
     )
     empty_t4 = EmptyOperator(
-        task_id="empty_t2",
+        task_id="empty_t4",
     )
     empty_t5 = EmptyOperator(
-        task_id="empty_t1",
+        task_id="empty_t5",
     )
     empty_t6 = EmptyOperator(
-        task_id="empty_t2",
+        task_id="empty_t6",
     )
     empty_t7 = EmptyOperator(
-        task_id="empty_t1",
+        task_id="empty_t7",
     )
     empty_t8 = EmptyOperator(
-        task_id="empty_t2",
+        task_id="empty_t8",
     )
 
     empty_t1 >> [empty_t2, empty_t3] >> empty_t4
