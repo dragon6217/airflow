5f9825dc49da40f5033e9f796b3b6b4894136e2b
Seongjung Um <232974616+dragon6217@users.noreply.github.com>
2026-01-01 09:19:31 +0900

    test modify for s04_l22

--- plugins/common/s04_l18_common_func.py
+++ plugins/common/s04_l18_common_func.py
@@ -15,6 +15,7 @@ def regist2(name, sex, *args, **kwargs):
     print(f'이름: {name}')
     print(f'성별: {sex}')
     print(f'기타옵션들: {args}')
+    print(f'전체 kwargs: {kwargs}')
     email = kwargs['email'] or None
     phone = kwargs['phone'] or None
     if email:
